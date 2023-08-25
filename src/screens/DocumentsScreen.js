03ebd9317ad1208e33b22db715f241acb6aa34f0
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-08-25 15:36:45 +0600

    pdf-added

--- src/screens/DocumentsScreen.js
+++ src/screens/DocumentsScreen.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { useTranslation, Trans } from "react-i18next";
-import Pdf from "../assets/Jintropine.pdf";
+// import Pdf from "../assets/Jintropine.pdf";
 import documentScreenshot from "../assets/Document.png";
 import documentScreenshotTwo from "../assets/Document2.png";
-import { Document, Page } from "react-pdf";
+// import { Document, Page } from "react-pdf";
 import { useNavigate } from "react-router-dom";
 import styles from "../styles/document-styles/document_styles.css";
+
+import document1 from "../pdf/document1.pdf";
+import document2 from "../pdf/document2.pdf";
 
 const DocumentsScreen = () => {
   const { t, i18n } = useTranslation();
@@ -31,23 +34,13 @@ const DocumentsScreen = () => {
               className="document-photo"
               src={documentScreenshot}
               alt="document"
-              onClick={() =>
-                window.open(
-                  "https://online.flippingbook.com/view/999607250/",
-                  "_blank"
-                )
-              }
+              onClick={() => window.open(document1, "_blank")}
             ></img>
             <img
               className="document-photo-2"
               src={documentScreenshotTwo}
               alt="document"
-              onClick={() =>
-                window.open(
-                  "https://online.flippingbook.com/view/999490080/",
-                  "_blank"
-                )
-              }
+              onClick={() => window.open(document2, "_blank")}
             ></img>
             {/* <img className="document-photo" src={document}></img> */}
           </div>
